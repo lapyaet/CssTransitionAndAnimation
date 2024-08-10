92c59a4f836bdafbcca95f2a8b1fcbdd1ac6a8b7
lapyae <lapyae@lapyaes-MBP.lan>
2024-08-10 18:49:27 +0700

    cssAnimation

---
+++ MousePositionAwareCircleFillHoverEffect/script.js
@@ -0,0 +1,10 @@
+// $(document).ready(function () {
+//   $(".button.first").on("mouseenter", function (event) {
+//     console.log(event);
+//   });
+// });
+
+const firstBtn = document.getElementById("firstBtn");
+firstBtn.addEventListener("mouseenter", (event) => {
+  console.log(event);
+});
---
+++ MoveBox/script.js
@@ -0,0 +1,3 @@
+function showOverlay() {
+  alert("hello");
+}
